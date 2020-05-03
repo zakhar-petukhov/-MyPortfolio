49bd2d6a3cd59f8e4df5c95d0a4fc13652c5984d
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-05-03 11:48:07 +0300

    :sparkles: create button for scrolling to top

---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,80 @@
+<template>
+    <a id="button" @click="topFunction" @scroll="scrollFunction"></a>
+</template>
+
+
+<script>
+export default {  
+    methods: {
+      handleScroll: function () {
+            var mybutton = document.getElementById("button");
+
+            if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+                mybutton.style.display = "block";
+
+            } else {
+                mybutton.style.display = "none";
+            }
+          },
+
+      topFunction : function() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        
+          }
+  },
+
+    created: function () {
+            window.addEventListener('scroll', this.handleScroll);
+        },
+
+    destroyed: function () {
+            window.removeEventListener('scroll', this.handleScroll);
+      }
+  }  
+
+
+</script>
+
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+
+
+#button {
+  display: none;
+  background-color: red;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  border-radius: 4px;
+  margin: 30px;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  transition: background-color .3s;
+  z-index: 1000;
+}
+
+#button:hover {
+  cursor: pointer;
+  background-color: #333;
+}
+
+#button:active {
+  background-color: #555;
+}
+
+#button::after {
+  content: "\f077";
+  font-family: FontAwesome;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 2em;
+  line-height: 50px;
+  color: #fff;
+}
+</style>
+
