8ebfbcdc25649aa56315be00df18ca9b54af9b37
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-05-05 07:49:34 +0300

    :sparkles: create contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="contact-me" id="contact-me">
+        <b-container>
+            <b-row align-v="center" align-h="center">
+                <b-col lg="10">
+                    <b-row align-h="between">
+                        <b-col md="10" align="center" data-aos="fade-down" data-aos-duration="600">
+                            <h2 :style='{ color: "black", fontWeight: "bold"}'>Get In Touch</h2>
+                            <h3>I am always ready for new acquaintances, whether it is something great or you just decide to tell about the last book you read. Write to me! Just do it :)</h3>
+                             <a href="mailto:zakharpetukhov@protonmail.com" class="btn btn-danger btn-lg" target="_blank">
+                                                <font-awesome-icon icon="envelope" class="mr-1"></font-awesome-icon> Say Hello</a>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-row>
+        </b-container>
+    </section>
+</template>
+
+
